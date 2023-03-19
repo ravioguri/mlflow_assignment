e9d57b0b5d9e82b602cb90c59766228bde1f6977
ravioguri <oguri.ravindra@tigeranalytics.com>
2023-03-20 00:35:51 +0530

    mlflow dashboard & experiment ceated

---
+++ housing_modelling.py
@@ -0,0 +1,209 @@
+import numpy as np
+import pandas as pd
+import os
+import tarfile
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import randint
+from sklearn.linear_model import LinearRegression
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error, r2_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeRegressor
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+
+from scipy.stats import randint
+from sklearn.impute import SimpleImputer
+
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = os.path.join("data")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+"""
+1. Try a Support Vector Machine regressor (sklearn.svm.SVR) with various hyperparameters, such as kernel="linear" (with various values for the C hyperparameter) or kernel="rbf" (with various values for the C and gamma hyperparameters). Don’t worry about what these hyperparameters mean for now. How does the best SVR predictor perform?
+
+2. Try replacing GridSearchCV with RandomizedSearchCV.
+
+3. Try adding a transformer in the preparation pipeline to select only the most important attributes.
+"""
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    os.makedirs(housing_path, exist_ok=True)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
+
+def model_train(housing, test_size=0.2):
+
+    train_set, test_set = train_test_split(
+        housing, test_size=test_size, random_state=42
+    )
+
+    housing["income_cat"] = pd.cut(
+        housing["median_income"],
+        bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
+        labels=[1, 2, 3, 4, 5],
+    )
+
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+    for train_index, test_index in split.split(housing, housing["income_cat"]):
+        strat_train_set = housing.loc[train_index]
+        strat_test_set = housing.loc[test_index]
+
+    def income_cat_proportions(data):
+        return data["income_cat"].value_counts() / len(data)
+
+    train_set, test_set = train_test_split(
+        housing, test_size=0.2, random_state=42
+    )
+
+    compare_props = pd.DataFrame(
+        {
+            "Overall": income_cat_proportions(housing),
+            "Stratified": income_cat_proportions(strat_test_set),
+            "Random": income_cat_proportions(test_set),
+        }
+    ).sort_index()
+    compare_props["Rand. %error"] = (
+        100 * compare_props["Random"] / compare_props["Overall"] - 100
+    )
+    compare_props["Strat. %error"] = (
+        100 * compare_props["Stratified"] / compare_props["Overall"] - 100
+    )
+
+    for set_ in (strat_train_set, strat_test_set):
+        set_.drop("income_cat", axis=1, inplace=True)
+
+    housing = strat_train_set.copy()
+    housing.plot(kind="scatter", x="longitude", y="latitude")
+    housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
+
+    corr_matrix = housing.corr()
+    corr_matrix["median_house_value"].sort_values(ascending=False)
+    housing["rooms_per_household"] = (
+        housing["total_rooms"] / housing["households"]
+    )
+    housing["bedrooms_per_room"] = (
+        housing["total_bedrooms"] / housing["total_rooms"]
+    )
+    housing["population_per_household"] = (
+        housing["population"] / housing["households"]
+    )
+
+    housing = strat_train_set.drop(
+        "median_house_value", axis=1
+    )  # drop labels for training set
+    housing_labels = strat_train_set["median_house_value"].copy()
+
+    imputer = SimpleImputer(strategy="median")
+
+    housing_num = housing.drop("ocean_proximity", axis=1)
+
+    imputer.fit(housing_num)
+    X = imputer.transform(housing_num)
+
+    housing_tr = pd.DataFrame(
+        X, columns=housing_num.columns, index=housing.index
+    )
+    housing_tr["rooms_per_household"] = (
+        housing_tr["total_rooms"] / housing_tr["households"]
+    )
+    housing_tr["bedrooms_per_room"] = (
+        housing_tr["total_bedrooms"] / housing_tr["total_rooms"]
+    )
+    housing_tr["population_per_household"] = (
+        housing_tr["population"] / housing_tr["households"]
+    )
+
+    housing_cat = housing[["ocean_proximity"]]
+    housing_prepared = housing_tr.join(
+        pd.get_dummies(housing_cat, drop_first=True)
+    )
+
+    lin_reg = LinearRegression()
+    lin_reg.fit(housing_prepared, housing_labels)
+
+    return lin_reg, strat_test_set
+
+
+def model_predict(lin_reg, strat_test_set):
+
+    housing = strat_test_set.copy()
+    housing.plot(kind="scatter", x="longitude", y="latitude")
+    housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
+
+    corr_matrix = housing.corr()
+    corr_matrix["median_house_value"].sort_values(ascending=False)
+    housing["rooms_per_household"] = (
+        housing["total_rooms"] / housing["households"]
+    )
+    housing["bedrooms_per_room"] = (
+        housing["total_bedrooms"] / housing["total_rooms"]
+    )
+    housing["population_per_household"] = (
+        housing["population"] / housing["households"]
+    )
+
+    housing = strat_test_set.drop(
+        "median_house_value", axis=1
+    )  # drop labels for training set
+    housing_labels = strat_test_set["median_house_value"].copy()
+
+    imputer = SimpleImputer(strategy="median")
+
+    housing_num = housing.drop("ocean_proximity", axis=1)
+
+    imputer.fit(housing_num)
+    X = imputer.transform(housing_num)
+
+    housing_tr = pd.DataFrame(
+        X, columns=housing_num.columns, index=housing.index
+    )
+    housing_tr["rooms_per_household"] = (
+        housing_tr["total_rooms"] / housing_tr["households"]
+    )
+    housing_tr["bedrooms_per_room"] = (
+        housing_tr["total_bedrooms"] / housing_tr["total_rooms"]
+    )
+    housing_tr["population_per_household"] = (
+        housing_tr["population"] / housing_tr["households"]
+    )
+
+    housing_cat = housing[["ocean_proximity"]]
+    housing_prepared = housing_tr.join(
+        pd.get_dummies(housing_cat, drop_first=True)
+    )
+
+    return lin_reg.predict(housing_prepared)  # housing_predictions
+
+
+def eval_metrics(housing_labels, housing_predictions):
+    lin_mse = mean_squared_error(housing_labels, housing_predictions)
+    lin_rmse = np.sqrt(lin_mse)
+
+    lin_mae = mean_absolute_error(housing_labels, housing_predictions)
+    r2 = r2_score(housing_labels, housing_predictions)
+
+    return r2, lin_rmse, lin_mae
